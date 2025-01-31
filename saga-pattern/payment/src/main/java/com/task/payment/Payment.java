7c5d0a28ce3b47665fd1b2385b86afc3d023f05f
xdeejm97 <d.niesmialowski@gmail.com>
2025-01-31 21:06:50 +0100

    Add config and entity of payment

---
+++ saga-pattern/payment/src/main/java/com/task/payment/Payment.java
@@ -0,0 +1,22 @@
+package com.task.payment;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "payments")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+}
